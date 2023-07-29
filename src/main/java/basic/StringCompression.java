2623331cc4b3f8e12218b0698df7165fe9f43a88
codingmatemoon <janghyun2478@gmail.com>
2023-07-29 10:09:08 +0900

    [프로그래머스] 문자열 압축 (진행3) https://school.programmers.co.kr/learn/courses/30/lessons/60057

--- src/main/java/basic/StringCompression.java
+++ src/main/java/basic/StringCompression.java
@@ -1,5 +1,8 @@
 package basic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCompression {
     public static int solution(String s) {
 
@@ -7,9 +10,8 @@ public class StringCompression {
         단위 unit 1 ~ n/2 까지 각각 압축했을 때 길이를 구하고 가장 짧은 경우에 대해 min으로 저장해서 return합니다.
         n = 10 일 경우 배열 기준 단위를 이루는 인덱스는 0 ~ 4(10/2 - 1[maxUnit - 1]), 비교군 인덱스는 5(10/2) ~ 9
         n = 9 일 경우 배열 기준 단위를 이루는 인덱스는 0 ~ 3(9/2 - 1[maxUnit - 1]), 비교군 인덱스는 4(9/2) ~ 8
-        unit 1 기준 : i == i + 1 인 경우 compressedCnt++, compressCnt 1일 때 다시 i = i + 1인 경우 length--
-        unit 2 기준 : elements 01 != 23 -> continue. 01 == 23 -> list에 저장 후 lenth += 2. 요소 하나 하나 진행할 때 마다 length를 구하는 등 필요한 작업을 처리합니다.
-        unit 단위로 리스트를 구하고 값이 같은 경우 count++, count > 1 일때 StringBuilder compressedString에 값을 추가합니다.
+        이전 단위 요소 last와 다음 단위 요소 token를 서로 비교하고 같은 경우 count를 늘리며 다른 경우 StringBuilder에 last값을 추가하고 token을 last에 입력. + count가 2 이상인 경우 해당값도 추가.
+        substring을 활용해서 unit 단위로 리스트를 구하고 리스트 내 항목들 간에 값이 서로 같은 경우 count++, count > 1 일때 StringBuilder compressedString에 값을 추가합니다.
          */
         char[] elements = s.toCharArray();
 
@@ -25,9 +27,45 @@ public class StringCompression {
 
     private static String compressString(String s, int unit) {
 
-        
-        for (int i= 0; i < s.length(); i+=unit) {
+
+        String last = "";
+        int index = 0;
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (String token : split(s, unit)) {
+            if (index == 0) {
+                last = token;
+                index++;
+                continue;
+            }
+
+            if (token.equals(last)) {
+                count++;
+                continue;
+            }
+
+            if (count > 1) {
+                sb.append(count);
+            }
+            sb.append(last);
+            last = token;
+            count = 0;
+
 
         }
     }
+
+    private static List<String> split(String s, int unit) {
+        List<String> tokens = new ArrayList<>();
+        for (int i = 0; i < s.length(); i+=unit) {
+            if ((i + unit) > s.length()) {
+                tokens.add(s.substring(i, s.length()));
+            } else {
+                tokens.add(s.substring(i, i + unit));
+            }
+        }
+        return tokens;
+    }
+
+
 }
