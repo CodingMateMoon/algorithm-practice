f99bb9f93b5d95334966f2dc76f891deb5100a53
codingmatemoon <janghyun2478@gmail.com>
2023-08-06 10:17:32 +0900

    [프로그래머스] 숫자 문자열과 영단어 (보완) https://school.programmers.co.kr/learn/courses/30/lessons/81301

--- src/main/java/basic/NumberStringAndWords.java
+++ src/main/java/basic/NumberStringAndWords.java
@@ -1,10 +1,11 @@
 package basic;
 
 public class NumberStringAndWords {
-    public static int solution(String s) {
+    private static final String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    public static int solution_1(String s) {
         char[] numberString = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        String[] words = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        //String[] words = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         int index = 0;
         for (int i = 0; i < numberString.length; i++) {
             if ('0' <= numberString[i] && numberString[i] <= '9') {
@@ -22,5 +23,12 @@ public class NumberStringAndWords {
 
         }
         return Integer.parseInt(sb.toString());
+    }
+
+    public static int solution(String s) {
+        for (int i = 0; i < words.length; i++) {
+            s = s.replace(words[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
     }
 }
