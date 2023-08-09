9ce75f056f1af1216c33d314dd0672a12a6c5e51
codingmatemoon <janghyun2478@gmail.com>
2023-08-10 00:30:57 +0900

    [프로그래머스] 문자열 다루기 기본 (보완) https://school.programmers.co.kr/learn/courses/30/lessons/12918

--- src/main/java/basic/StringHandling.java
+++ src/main/java/basic/StringHandling.java
@@ -1,10 +1,25 @@
 package basic;
 
 public class StringHandling {
-    public static boolean solution(String s) {
+    public static boolean solution_1(String s) {
         if ((s.length() == 4 || s.length() == 6) && s.matches("[0-9]*")) {
             return true;
         }
         return false;
+    }
+
+    public static boolean solution_2(String s) {
+        return (s.matches("[0-9]{4}||[0-9]{6}"));
+    }
+
+    public static boolean solution(String s) {
+        if (s.length() !=4 && s.length() != 6)
+            return false;
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
