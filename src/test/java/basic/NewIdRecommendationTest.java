d7e37f67e4bb3fee479edb9c2f1e8ef65bc2980c
codingmatemoon <janghyun2478@gmail.com>
2023-08-12 09:23:44 +0900

    [프로그래머스] 신규 아이디 추천 (완료) https://school.programmers.co.kr/learn/courses/30/lessons/72410

--- src/test/java/basic/NewIdRecommendationTest.java
+++ src/test/java/basic/NewIdRecommendationTest.java
@@ -15,7 +15,6 @@ class NewIdRecommendationTest {
     6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
          만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
     7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-    https://school.programmers.co.kr/learn/courses/30/lessons/72410
      */
     @Test
     @DisplayName("신규 아이디 추천")
@@ -30,10 +29,10 @@ class NewIdRecommendationTest {
     }
 
     class NewIdRecommendation {
-        public static String solution(String s) {
+        public static String solution(String newId) {
             StringBuilder sb = new StringBuilder();
 
-            for (char c : s.toCharArray()) {
+            for (char c : newId.toCharArray()) {
                 if ('A' <= c && c <= 'Z') {
                     c = (char) (c + ('a' - 'A'));
                     sb.append(c);
@@ -44,9 +43,52 @@ class NewIdRecommendationTest {
                     sb.append(c);
                 }
             }
-            String filteredString = sb.toString();
-            System.out.println(filteredString);
-            return null;
+
+            newId = sb.toString();
+            sb.setLength(0);
+            boolean isPeriod = false;
+            for (char c : newId.toCharArray()) {
+                if (c == '.') {
+                    if (isPeriod) {
+                        continue;
+                    }
+                    isPeriod = true;
+                    sb.append(c);
+                    continue;
+                }
+                isPeriod = false;
+                sb.append(c);
+            }
+
+            newId = sb.toString();
+            if (newId != null && newId.length() > 0 && newId.charAt(0) == '.') {
+                newId = newId.substring(1);
+            }
+            if (newId != null && newId.length() > 0 && newId.charAt(newId.length() - 1) == '.') {
+                newId = newId.substring(0, newId.length() - 1);
+            }
+            if (newId == null || newId.length() == 0) {
+                newId = "a";
+            }
+
+            if (newId.length() >= 16) {
+                newId = newId.substring(0, 15);
+                if (newId.charAt(newId.length() - 1) == '.') {
+                    newId = newId.substring(0, newId.length() - 1);
+                }
+            }
+
+            if (newId.length() <= 2) {
+                sb = new StringBuilder(newId);
+                char c = newId.charAt(newId.length() - 1);
+                while (sb.length() < 3) {
+                    sb.append(c);
+                }
+
+                newId = sb.toString();
+            }
+
+            return newId;
         }
     }
 }
