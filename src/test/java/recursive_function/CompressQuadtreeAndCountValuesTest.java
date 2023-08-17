b30d708accd5ca9205799e43d6b5bac82ad3f5e4
codingmatemoon <janghyun2478@gmail.com>
2023-08-18 08:32:01 +0900

    [프로그래머스] 쿼드압축 후 개수 세기 (진행2) https://school.programmers.co.kr/learn/courses/30/lessons/68936

--- src/test/java/recursive_function/CompressQuadtreeAndCountValuesTest.java
+++ src/test/java/recursive_function/CompressQuadtreeAndCountValuesTest.java
@@ -12,7 +12,7 @@ public class CompressQuadtreeAndCountValuesTest {
         4등분으로 나누고 값이 전부 1인 경우 1 한 개, 전부 0인 경우 0 한 개로 count 합니다.
         (1) n = 1, 길이가 2x2일 때 1,0이 섞여있어서 압축이 불가능한 상태까지 나눈 경우, 값이 모두 0 또는 1인 경우에 대해 return (zeroCount, oneCount)
         (2) 2^n - e.g, 2^3일 때 2^2 4개의 사각형으로 나누고 각 사각형에서 모든 수가 같은 값인지 확인
-        (3) 0 ~ 2^n-1, 2^n-1 ~ 2^n 모두 같은 값(0 또는 1)인지 계산. 0 ~ x/2, x/2 ~ x
+        (3) (0,0) ~ (2^n-1, 2^n-1), (2^n-1,0) ~ (2^n-1,2^n), (0, 2^n-1) ~ (2^n-1, 2^n), (2^n-1, 2^n-1)~(2^n, 2^n) 모두 같은 값(0 또는 1)인지 계산
         상태 (n, n)
          */
         // given
@@ -27,7 +27,7 @@ public class CompressQuadtreeAndCountValuesTest {
             return new int[0];
         }
 
-        public static int[] calculateQuadtreeValue(int[][] arr, int startIndex, int endIndex) {
+        public static int[] calculateQuadtreeValue(int[][] arr, int n, int offsetX, int offsetY) {
 
             int length = 1;
             int zeroCount = 0;
@@ -52,11 +52,6 @@ public class CompressQuadtreeAndCountValuesTest {
                 }
             }
 
-
-            calculateQuadtreeValue(arr, 0, n / 2);
-            calculateQuadtreeValue(arr,  n / 2,  n);
-
-
             if (isCompressed) {
                 if (standardNumber == 0) {
                     zeroCount++;
@@ -72,6 +67,15 @@ public class CompressQuadtreeAndCountValuesTest {
             return new int[]{0,0};
         }
 
+        public class Count {
+            final int zero;
+            final int one;
+
+            public Count(int zero, int one) {
+                this.zero = zero;
+                this.one = one;
+            }
+        }
 
     }
 }
