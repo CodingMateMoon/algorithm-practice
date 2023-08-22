3066c022820462c24589981434cccd92c86371b3
codingmatemoon <janghyun2478@gmail.com>
2023-08-23 08:45:39 +0900

    [프로그래머스] 모음사전 (완료) https://school.programmers.co.kr/learn/courses/30/lessons/84512

--- src/test/java/recursive_function/VowelDictionaryTest.java
+++ src/test/java/recursive_function/VowelDictionaryTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VowelDictionaryTest {
     /*
     A, E, I, O, U 를 이용하여 길이 5이하인 단어를 구성할 때 A, AA, AAA ...부터 시작해서 AE, AI, AO, AU 등으로 끝날 수 있습니다.
@@ -15,7 +18,9 @@ public class VowelDictionaryTest {
     1113 - 8
     1114 - 9
     1115 - 10
-
+    (word)
+    if (word.length >= 5) return
+    word + A, word + E, word + I, word + O, word + U
      */
     @Test
     @DisplayName("모음사전")
@@ -27,15 +32,23 @@ public class VowelDictionaryTest {
     }
 
     private static class VowelDictionary {
-        public static int solution(String word) {
 
-            getWordSequence(word, 1, 1);
-            return 0;
+        static final char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+
+        public static int solution(String word) {
+            List<String> words = new ArrayList<>();
+            getWords("", words);
+            return words.indexOf(word);
         }
 
-        private static int getWordSequence(String word, int length, int x) {
-            int count = 0;
-            return 0;
+        private static void getWords(String word, List<String> words) {
+            words.add(word);
+            if (word.length() >= 5) {
+                return;
+            }
+            for (char c : vowels) {
+                getWords(word + c, words);
+            }
         }
     }
 }
