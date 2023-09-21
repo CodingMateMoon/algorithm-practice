2729643c33f9ac7afae8023cc419be0141683e07
codingmatemoon <abc9529@naver.com>
2023-09-22 08:56:59 +0900

    [프로그래머스] 불량 사용자 (진행2)

--- src/test/java/brute_force/BadUserTest.java
+++ src/test/java/brute_force/BadUserTest.java
@@ -7,6 +7,14 @@ import org.junit.jupiter.api.Test;
 public class BadUserTest {
 
     // https://school.programmers.co.kr/learn/courses/30/lessons/64064
+    /*
+    1 <= user_id length <= 8
+    1 <= user_id[i] : 중복X, 소문자, 숫자로만 구성 <= 8
+    1 <= banned_id.length <= user_id.length
+    1 <= banned_id[i] : 소문자, 숫자, *로 구성. 최소 * 1개 이상, 응모자 아이디 1개와 매핑. 중복 매핑 X <= 8
+    제재 아이디 목록 조합(나열 순서 상관X)
+
+     */
     @Test
     @DisplayName("BadUserTest")
     public void badUserTest() throws Exception{
