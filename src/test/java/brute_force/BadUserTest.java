6351cdd005645d149e6e9d98e6c09e4edb13e6df
codingmatemoon <abc9529@naver.com>
2023-09-29 00:00:14 +0900

    [프로그래머스] 불량 사용자 (진행9)

--- src/test/java/brute_force/BadUserTest.java
+++ src/test/java/brute_force/BadUserTest.java
@@ -26,30 +26,34 @@ public class BadUserTest {
         fr*d* -> frodo, fradi
         abc1** -> abc123
          */
-        Assertions.assertThat(BadUser.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "abc1**"})).isEqualTo(2);
+        //Assertions.assertThat(BadUser.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "abc1**"})).isEqualTo(2);
         Assertions.assertThat(BadUser.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"*rodo", "*rodo", "******"})).isEqualTo(2);
         /* frodo, crodo, abc123, frodoc | fradi, crodo, abc123, frodoc | fradi, frodo, abc123, frodoc
         fr*d*-> frodo, fradi
         *rodo-> frodo, crodo
         ******-> abc123, frodoc
          */
-        Assertions.assertThat(BadUser.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "*rodo", "******", "******"})).isEqualTo(3);
+        //Assertions.assertThat(BadUser.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "*rodo", "******", "******"})).isEqualTo(3);
     }
 
     private static class BadUser {
 
-        static Boolean[] isBannedIdSelected;
+        static boolean[] isBannedIdSelected;
+        static int caseCount;
 
         public static int solution(String[] userIds, String[] bannedIds) {
 
-            Boolean[] isSelected = new Boolean[userIds.length];
-            isBannedIdSelected = new Boolean[bannedIds.length];
+            boolean[] isUserIdSelected = new boolean[userIds.length];
+            isBannedIdSelected = new boolean[bannedIds.length];
+            caseCount = 0;
 
-            List<String> badUsers = getBadUser(userIds, bannedIds, isSelected, new ArrayList<String>());
-            return 0;
+
+            getBadUser(userIds, bannedIds, isUserIdSelected, new ArrayList<String>());
+            System.out.println("solution : " + caseCount);
+            return caseCount;
         }
 
-        private static List<String> getBadUser(String[] userIds, String[] bannedIds, Boolean[] isSelected, List<String> badUsers) {
+        private static void getBadUser(String[] userIds, String[] bannedIds, boolean[] isUserIdSelected, List<String> badUsers) {
 
              /*
              Assertions.assertThat(BadUser.solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"*rodo", "*rodo", "******"})).isEqualTo(2);
@@ -57,7 +61,7 @@ public class BadUserTest {
              *rodo-> frodo, crodo
              *rodo-> frodo, crodo
              ******-> abc123, frodoc
-             1 : 응모자 아이디 목록에서 불량사용자1과 매핑되는 요소 1개 선택. 응모자 아이디 isSelected = true
+             1 : 응모자 아이디 목록에서 불량사용자1과 매핑되는 요소 1개 선택. 응모자 아이디 isSelected = true. getBadUser 재귀호출.
              2 : 1번에서 제외하고 남은 응모자 아이디 목록 중 불량사용자2와 매핑되는 요소 1개 선택
              3 : 1,2에서 제외하고 남은 응모자 아이디 목록 중 불량사용자3과 매핑되는 요소 1개 선택
              => 재귀 for문 활용 고려
@@ -66,22 +70,61 @@ public class BadUserTest {
              10101(frodo:true, crodo:true, frodoc:true)
              불량사용자 기준 1,2,3 ~ 다 true일 때 print
              */
-            for (int bannedIdOffset = 0; bannedIdOffset < bannedIds.length; bannedIdOffset++) {
-                if(isBannedIdSelected[bannedIdOffset]){
-                    continue;
-                }
 
-                for (int userIdOffset = 0; userIdOffset < userIds.length; userIdOffset++) {
-                    if (isSelected[userIdOffset]) {
-                        continue;
-                    }
-                    char[] charBannedId = bannedIds[bannedIdOffset].toCharArray();
-                    char[] charUserId = userIds[userIdOffset].toCharArray();
-                }
+            if (badUsers.size() == bannedIds.length) {
+                System.out.println("completed : " + badUsers);
+                caseCount++;
+                System.out.println("caseCount : " + caseCount);
+                return;
             }
 
 
-            return null;
+            for (int bannedIdOffset = 0; bannedIdOffset < bannedIds.length; bannedIdOffset++) {
+                if (isBannedIdSelected[bannedIdOffset]) {
+                    continue;
+                }
+
+
+                for (int userIdOffset = 0; userIdOffset < userIds.length; userIdOffset++) {
+                    if (isUserIdSelected[userIdOffset]) {
+                        continue;
+                    }
+
+                    if (bannedIds[bannedIdOffset].length() != userIds[userIdOffset].length())
+                        continue;
+
+                    char[] charBannedId = bannedIds[bannedIdOffset].toCharArray();
+                    char[] charUserId = userIds[userIdOffset].toCharArray();
+
+                    boolean isSame = true;
+                    for (int charBannedIdOffset = 0; charBannedIdOffset < charBannedId.length; charBannedIdOffset++) {
+
+                        if(charBannedId[charBannedIdOffset] == '*')
+                            continue;
+
+                        if (charBannedId[charBannedIdOffset] != charUserId[charBannedIdOffset]) {
+                            isSame = false;
+                            System.out.println("(userId : " + userIdOffset + ") / (bannedId : " + bannedIdOffset + ") / " + userIds[userIdOffset] + " : " + bannedIds[bannedIdOffset]);
+                        }
+                    }
+
+                    if (isSame) {
+                        isBannedIdSelected[bannedIdOffset] = true;
+                        isUserIdSelected[userIdOffset] = true;
+                        System.out.println(bannedIdOffset + " : " + userIds[userIdOffset] + " : " + bannedIds[bannedIdOffset]);
+                        badUsers.add(userIds[userIdOffset]);
+                        System.out.println(badUsers);
+
+                        getBadUser(userIds, bannedIds, isUserIdSelected, badUsers);
+                        isBannedIdSelected[bannedIdOffset] = false;
+                        badUsers.remove(userIds[userIdOffset]);
+                        isUserIdSelected[userIdOffset] = false;
+                    } else {
+                        return;
+                    }
+                }
+
+            }
         }
     }
 }
