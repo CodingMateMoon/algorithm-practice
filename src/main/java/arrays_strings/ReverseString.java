14f61dd60018248fe9806b5528ed7decd4a75187
codingmatemoon <janghyun2478@gmail.com>
2025-01-03 14:13:55 +0900

    [leetcode] Reverse String [done]

--- src/main/java/arrays_strings/ReverseString.java
+++ src/main/java/arrays_strings/ReverseString.java
@@ -2,6 +2,16 @@ package arrays_strings;
 
 public class ReverseString {
     public static char[] reverseString(char[] s) {
-        return "olleh".toCharArray();
+
+        int start_index = 0;
+        int last_index = s.length - 1;
+        while (start_index < last_index) {
+           char temp = s[start_index];
+           s[start_index] = s[last_index];
+           s[last_index] = temp;
+           start_index++;
+           last_index--;
+        }
+        return s;
     }
 }
