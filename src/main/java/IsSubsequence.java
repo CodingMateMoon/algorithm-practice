2a42fe511750dc082d8e07bb92a0207e709e78c0
codingmatemoon <janghyun2478@gmail.com>
2025-01-09 10:07:11 +0900

    [leetcode] is-subsequence [doing2]

--- src/main/java/IsSubsequence.java
+++ src/main/java/IsSubsequence.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 // https://leetcode.com/problems/is-subsequence/
 /*
 
@@ -25,10 +27,16 @@ Constraints:
 s and t consist only of lowercase English letters.
 
 s의 요소가 순서대로 t의 요소에 매핑되는 것이 있는지 확인. O(n^2) 의 경우 100^104으로 값이 커서 불가.
-
+a, h 등 값을 키, index를 value로 저장하는 hash 생성 후 key가 존재하고 key에 매핑되는 index 위치기 이전 index 위치보다 클 경우
+a b c
  */
 public class IsSubsequence {
     public boolean isSubsequence(String s, String t) {
+
+        for (int i = 0; i < t.length(); i++) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            map.put(t.charAt(i), i);
+        }
 
     }
 }
