51f8ddb13e92cee94f16511705b57889e8ac00db
codingmatemoon <janghyun2478@gmail.com>
2025-03-20 19:01:50 +0900

    [leetcode] SquaresOfaSortedArray[done|two pointers]

--- src/main/java/arrays_strings/SquaresOfaSortedArray.java
+++ src/main/java/arrays_strings/SquaresOfaSortedArray.java
@@ -46,13 +46,33 @@ public class SquaresOfaSortedArray {
         }
         return result;
     }
-    public int[] sortedSquares(int[] nums) {
+    public int[] sortedSquares_2(int[] nums) {
 
         int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             result[i] = nums[i] * nums[i];
         }
         Arrays.sort(result);
+        return result;
+    }
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+
+            result[i] = square * square;
+        }
         return result;
     }
 }
